a3ca5f78fddee5c05a39b6e91adc2a944c5679f3
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-19 18:17:54 +1030

    created program functionality

---
+++ delete-file.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+   if (remove("../../file.txt") == 0)
+      printf("Deleted successfully");
+   else
+      printf("Unable to delete the file");
+
+   return 0;
+}
